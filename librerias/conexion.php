2d6e9da3e0638cbe30406510a767504faebfc059
Ivan Diaz <ivancho2802@hotmail.com>
2019-09-02 23:29:18 -0500

    x

--- librerias/conexion.php
+++ librerias/conexion.php
@@ -2,7 +2,14 @@
 // $conexion = new mysqli("ec2-107-20-155-148.compute-1.amazonaws.com", "kjogxhvdvnkumx", "b42f2b9f3dda672e63925904f1450b38698f03289409be8008efd07f526c20a9", "delcqdglr7h1b8", 5432);
 $conexion = mysqli_connect("ec2-107-20-155-148.compute-1.amazonaws.com", "kjogxhvdvnkumx", "b42f2b9f3dda672e63925904f1450b38698f03289409be8008efd07f526c20a9", "delcqdglr7h1b8", 5432);
 // $conexion = new mysqli("localhost", "root", "", "panaderia");
-if ($conexion->connect_errno) {
-    echo "Fallo al conectar a MySQL: (" . $conexion->connect_errno . ") " . $conexion->connect_error;
-}	
+// if ($conexion->connect_errno) {
+//     echo "Fallo al conectar a MySQL: (" . $conexion->connect_errno . ") " . $conexion->connect_error;
+// }
+if (!$conexion) {
+    die('Error de Conexión (' . mysqli_connect_errno() . ') '
+            . mysqli_connect_error());
+}
+
+echo 'Éxito... ' . mysqli_get_host_info($conexion) . "\n";
+mysqli_close($conexion);	
 ?>
