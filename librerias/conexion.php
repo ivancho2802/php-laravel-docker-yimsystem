3e3beda9cf8ed2630a3ec722c3eee9667b49aeb1
Ivan Diaz <ivancho2802@hotmail.com>
2019-09-02 22:54:25 -0500

    x

--- librerias/conexion.php
+++ librerias/conexion.php
@@ -1,5 +1,6 @@
 <?php
-$conexion = new mysqli("localhost", "root", "", "panaderia");
+$conexion = new mysqli("ec2-107-20-155-148.compute-1.amazonaws.com", "kjogxhvdvnkumx", "b42f2b9f3dda672e63925904f1450b38698f03289409be8008efd07f526c20a9", "delcqdglr7h1b8");
+// $conexion = new mysqli("localhost", "root", "", "panaderia");
 if ($conexion->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $conexion->connect_errno . ") " . $conexion->connect_error;
 }
