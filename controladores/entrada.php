65658d2499bebb65de45704d672924c70bcd662c
Ivan Diaz <ivancho2802@hotmail.com>
2019-09-03 00:12:38 -0500

    x

--- controladores/entrada.php
+++ controladores/entrada.php
@@ -3,8 +3,8 @@ ob_start();
 session_start();
 //////////////////////////////////			CONSULTA DE LA INFO DEL SISTEMA
 require_once("../librerias/conexion.php");
-// $sql="select * from data_system where edo_ds='1'";
-// $ok=pg_query($conexion, $sql);
+$sql="select * from data_system where edo_ds='1'";
+$ok=pg_query($conexion, $sql);
 //$resultado=mysql_fetch_assoc($ok);
 //////////////////////////////////////////
 require_once("../clases/usuario.class.php");
