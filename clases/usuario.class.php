f1640bbc99b90e88771f5b819154913cba90fe40
Ivan Diaz <ivancho2802@hotmail.com>
2019-09-12 11:37:11 -0500

    solicion de conflictos

--- clases/usuario.class.php
+++ clases/usuario.class.php
@@ -6,7 +6,7 @@ class usuario
 		$sql="select * from usuarios, data_system where usuario='$usuario' and password='$clave' and edo_ds=1";
 		$ok=pg_query($conexion,$sql);//mysql_query($sql);
 		// $ok=mysqli_query($conexion,$sql);//mysql_query($sql);
-		echo $ok; 
+		// echo $ok; 
 		
 		if($resultado = $ok->fetch_array())
 		{
