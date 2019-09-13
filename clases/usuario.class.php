3ee1a8b4ac471925a50cb6f8111c209b0bcfb6ac
Ivan Diaz <ivancho2802@hotmail.com>
2019-09-12 21:40:56 -0500

    cambios valid login

--- clases/usuario.class.php
+++ clases/usuario.class.php
@@ -6,10 +6,10 @@ class usuario
 		$sql="select * from usuarios, data_system where usuario='$usuario' and password='$clave' and edo_ds=1";
 		$ok=pg_query($conexion,$sql);//mysql_query($sql);
 		// $ok=mysqli_query($conexion,$sql);//mysql_query($sql);
-		var_dump($ok); 
+		// var_dump($ok); 
 		
 		// if($resultado = $ok->fetch_array())
-		echo($resultado = pg_fetch_row($ok));
+		// echo($resultado = pg_fetch_row($ok));
 		if($resultado = fetch_assoc($ok))
 		{
 			$resultado["mensaje"] = "";
