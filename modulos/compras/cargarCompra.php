80ddc21729256121481ecfa12311085b888c94b5
Ivan Diaz <ivancho2802@hotmail.com>
2019-09-24 21:26:38 -0500

    x

--- modulos/compras/cargarCompra.php
+++ modulos/compras/cargarCompra.php
@@ -7,9 +7,6 @@
 	$extra = 'index.php';
 	echo $host."<br />".$uri."<br />".$extra;
 	*/
-
-	echo "$extra";
-	echo $extra;
 ?>
 <!--PARA LLAMAR A EL cRUL RIF-->
 <script>
@@ -474,27 +471,28 @@ function mConsulFact(str){
     $filas=pg_fetch_assoc($consulta);
 	// $resEmpreActiva=$consulEmpreActiva->fetch_assoc();
 	$total_consulEmpreActiva = pg_num_rows($consulta);
+	$extra = "../../";
 	
 	//llamado de modales el id es "busFact"
-	include_once("../../modales/fact_c/m_b_fact_c.php");
+	include_once($extra."modales/fact_c/m_b_fact_c.php");
 	//llamado de modales el id es "mostrarFact"
 	//esta en el id res_nfact_afectada aqui
 	
 	//llamando de modales el id es "nueProv"
-	include_once(dirname(__DIR__).$extra."prov/m_a_prov.php");
+	include_once($extra."modales/prov/m_a_prov.php");
 	//llamado de modales el id es "busProv"
-	include_once(dirname(__DIR__).$extra."prov/m_b_prov.php");
+	include_once($extra."modales/prov/m_b_prov.php");
 	//llamando de modales el id es "modProv"
-	include_once(dirname(__DIR__).$extra."prov/m_m_prov.php");
+	include_once($extra."modales/prov/m_m_prov.php");
 	
 	//llamado de modales el id es "busProd"
-	include_once(dirname(__DIR__).$extra."prod/m_b_prod.php");
+	include_once($extra."modales/prod/m_b_prod.php");
 	//llamando de modales el id es "nueProd"
-	include_once(dirname(__DIR__).$extra."prod/m_a_prod.php");
+	include_once($extra."modales/prod/m_a_prod.php");
 	//llamando de modales el id es "mmProd"
-	include_once(dirname(__DIR__).$extra."prod/m_m_prod.php");
+	include_once($extra."modales/prod/m_m_prod.php");
 	//llamando de modales el id es "calPMPVJ"
-	include_once(dirname(__DIR__).$extra."prod/m_PMPVJ.php");
+	include_once($extra."modales/prod/m_PMPVJ.php");
 	
 	
 ?>
