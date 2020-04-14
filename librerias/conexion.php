2eb663fe1190b6ed70ed9b37547d048a21bac46d
ivancho2802 <ivancho2802@hotmail.com>
2020-04-14 10:08:21 -0400

    revision de la conexxion

--- librerias/conexion.php
+++ librerias/conexion.php
@@ -1,6 +1,7 @@
 <?php
 // $conexion = new mysqli("ec2-107-20-155-148.compute-1.amazonaws.com", "kjogxhvdvnkumx", "b42f2b9f3dda672e63925904f1450b38698f03289409be8008efd07f526c20a9", "delcqdglr7h1b8", 5432);
-$conexion =  pg_connect("host=ec2-107-20-155-148.compute-1.amazonaws.com port=5432 dbname=delcqdglr7h1b8 user=kjogxhvdvnkumx password=b42f2b9f3dda672e63925904f1450b38698f03289409be8008efd07f526c20a9") ;
+//$conexion =  pg_connect("host=ec2-107-20-155-148.compute-1.amazonaws.com port=5432 dbname=delcqdglr7h1b8 user=kjogxhvdvnkumx password=b42f2b9f3dda672e63925904f1450b38698f03289409be8008efd07f526c20a9") ;
+$conexion =  pg_connect("host=ec2-54-86-170-8.compute-1.amazonaws.com port=5432 dbname=d1ffioj5igskat user=nnogiraqfwogcp password=520027befc2d0ea9adc42f7f9a304c4ac4e202018532495c0417e26d4ef94d8e") ;
 // $conexion = new mysqli("localhost", "root", "", "panaderia");
 // if ($conexion->connect_errno) {
 //     echo "Fallo al conectar a MySQL: (" . $conexion->connect_errno . ") " . $conexion->connect_error;
