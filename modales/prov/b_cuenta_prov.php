9d074242d5bbb6c965bf63ea7da5672e68902226
ivancho2802 <ivancho2802@hotmail.com>
2020-04-16 11:05:06 -0400

    cambios test errores

--- modales/prov/b_cuenta_prov.php
+++ modales/prov/b_cuenta_prov.php
@@ -12,7 +12,7 @@ if(isset($_GET['cuenta']) ){
                                                         categ_cuenta.id = '%s' OR
                                                         nombre like '%%%s%' OR
                                                         descripcion LIKE '%%%s%';",
-                                                                    $_GET['cuenta'], $_GET['cuenta'], $_GET['cuenta']));
+                                                                    $_GET['cuenta'], $_GET['cuenta'], $_GET['cuenta'], $_GET['cuenta']));
   //mysql_select_db($database_conexPana, $conexPana);
   //SI NO SE REGISTR RETORNA DIE O MUERE EL PROCESO Y MUESTRA
   $consulta or die('
