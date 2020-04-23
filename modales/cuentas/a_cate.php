5b3ca44b6e5c8ef7749313f4b79807c74cf098eb
ivancho2802 <ivancho2802@hotmail.com>
2020-04-23 19:06:10 -0400

    agregar consultar categorias

--- modales/cuentas/a_cate.php
+++ modales/cuentas/a_cate.php
@@ -4,7 +4,7 @@ include_once('../../includes_SISTEM/include_login.php');
 
 //CODIGO DE INSERCION
 if(isset($_POST['id']) && isset($_POST['nombre']) && isset($_POST['descripcion'])  ){
-  $insertSQL = sprintf("INSERT INTO categoria (id, nombre, descrpcion) VALUES ('%s', '%s', '%s' )",
+  $insertSQL = sprintf("INSERT INTO categoria (id, nombre, descripcion) VALUES ('%s', '%s', '%s' )",
                        $_POST['id'], 
                        $_POST['nombre'], 
                        $_POST['descripcion']);
