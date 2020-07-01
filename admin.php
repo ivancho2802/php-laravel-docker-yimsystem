a7129224bd51e9556ff778b7f92a5958361f16ce
ivancho2802 <ivancho2802@hotmail.com>
2020-07-01 11:15:33 -0400

    init menu

--- admin.php
+++ admin.php
@@ -17,7 +17,7 @@ $okmenu=pg_query($conexion,$sqlmenu);
 $resultadomenu=pg_fetch_assoc($okmenu);
 $totalRows_Recordset1 = pg_num_rows($okmenu);
 $i = 0;
-$menubefore=array('-1' => 'inicial' );
+$menubefore=array('-1' => '0' );
 // var_dump($resultadomenu);
 ?>
 <!DOCTYPE HTML>
