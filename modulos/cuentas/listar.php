300bd7524c9aad4b452a7bf8532cfbe7953f7617
ivancho2802 <ivancho2802@hotmail.com>
2020-07-03 17:03:48 -0400

    estilos de consulta

--- modulos/cuentas/listar.php
+++ modulos/cuentas/listar.php
@@ -21,28 +21,29 @@
     $total_consulta = pg_num_rows($consulta);
 ?>
 <div id="cuentas"  class="bs-example">
-    <div class="">
-        <h1 class="bd-title">Cuentas </h1>
-    </div>
         <!-- col-xs-12 col-md-12 col-lg-12 -->
         <div class="form-group row">
-          <form method="POST" class="col-xs-12 col-md-12 col-lg-12">
-              <label class="control-label">Consulta Por Numero, nombre y categorias</label>
-              <div class="input-group">  
-                    <input type="text" class="form-control" name="cuenta" id="cuenta" required="required" lang="si-general">
-                    <span class="input-group-btn">
-                        <button  name="Consultar" id="Consultar" class="btn btn-primary" type="button"  value="">Buscar</button>
-                    </span> 
-              </div>
-          </form>
-          <form method="POST" action="listar_submit">
-            <button class="list-group-item list-group-item-secondary" type="button" onclick="modaladdcuenta()">Crear Cuenta!</button>
-            <!-- <button class="btn btn-primary" type="button" click="loadcrearcuenta()">Crear Cuenta!</button> -->
-          </form>
+
+            <div class="col-xs-6 col-md-6 col-lg-6">
+                <h1 class="bd-title">Cuentas </h1>
+            </div>
+              <form method="POST" class="col-xs-6 col-md-6 col-lg-6">
+                  <label class="control-label">Consulta Por Numero, nombre y categorias</label>
+                  <div class="input-group">  
+                        <input type="text" class="form-control" name="cuenta" id="cuenta" required="required" lang="si-general">
+                        <span class="input-group-btn">
+                            <button  name="Consultar" id="Consultar" class="btn btn-primary" type="button"  value="">Buscar</button>
+                        </span> 
+                  </div>
+              </form>
         </div>
 
 </div><!--cuentas-->
 <hr id="res_cuentas" class="featurette-divider"/>
+  <form method="POST" action="listar_submit">
+    <button type="button" class="btn btn-sm btn-primary col-xs-12 col-lg-12 glyphicon glyphicon-plus" onclick="modaladdcuenta()"></button>
+    <!-- <button class="btn btn-primary" type="button" click="loadcrearcuenta()">Crear Cuenta!</button> -->
+  </form>
 <?php
     //validando que la fecha o ano que se introduzca no sea menor al menor del sistema
     $param = isset($_POST['cuenta']) ? $_POST['cuenta'] :'';
