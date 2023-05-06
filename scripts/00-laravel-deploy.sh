295960ad1ff7a3bc0dc8d3b45daeb1f300d0d6df
ivan diaz <idiaz@scharfteinlabs.com>
2023-05-06 07:31:11 -0500

    fix error deploy docker + laravel

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,6 +2,7 @@
 echo "Running composer"
 composer global require hirak/prestissimo
 rm -rf vendor
+rm -rf composer.lock
 composer install --no-dev --working-dir=/var/www/html
 
 echo "Caching config..."
