6988dc4eb69f7157a62eb446c2d0fe5109f7ead4
ivan diaz <idiaz@scharfteinlabs.com>
2023-05-06 08:19:12 -0500

    fix error deploy docker + laravel

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -4,6 +4,7 @@ pwd
 rm -rf vendor
 rm -rf composer.lock
 echo "DELETE composer pwd"
+COPY --from=composer:1 /usr/bin/composer /usr/bin/composer
 composer global require hirak/prestissimo
 
 composer install --no-dev --working-dir=/var/www/html
