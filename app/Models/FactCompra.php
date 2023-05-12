44735a8c0ce9ea814907883ee024a9c4642004f2
ivan diaz <idiaz@scharfteinlabs.com>
2023-05-11 20:29:21 -0500

    se agrequita version1 y se soluciona error modelo fact

--- app/Models/FactCompra.php
+++ app/Models/FactCompra.php
@@ -2,13 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 //use App\Traits\HasConstants;
 
 class FactCompra extends Model
 {
-    use HasFactory;
 
     //use HasConstants;
 
